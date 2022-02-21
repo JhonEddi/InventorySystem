90c98bb4fee9fc5a9252501b2ae19b83ba26bef0
JhonEddi <31699155+JhonEddi@users.noreply.github.com>
2022-02-21 14:52:42 -0500

    Modificacion guardar registros back-end

--- Back/InventorySystem/src/main/java/InventorySystem/REST/RegistroREST.java
+++ Back/InventorySystem/src/main/java/InventorySystem/REST/RegistroREST.java
@@ -13,30 +13,32 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequestMapping("/registro/")
 public class RegistroREST {
-    
+
     @Autowired
     private RegistroService registroService;
-    
+
     @GetMapping
-    private ResponseEntity<List<RegistroDTO>> getAllRegistros(){
+    private ResponseEntity<List<RegistroDTO>> getAllRegistros() {
         log.info("Ejecutando controlador Spring MVC");
         return ResponseEntity.ok(registroService.listarRegistros());
     }
-    
+
     @GetMapping("{idUsuario}")
-    private ResponseEntity<List<RegistroDTO>> getAllRegistrosByUsuario(@PathVariable("idUsuario") int idUsuario){
+    private ResponseEntity<List<RegistroDTO>> getAllRegistrosByUsuario(@PathVariable("idUsuario") int idUsuario) {
         log.info("Ejecutando controlador Spring MVC");
         return ResponseEntity.ok(registroService.listarPorUsuario(idUsuario));
     }
-    
+
     @GetMapping("{idMercancia}")
-    private ResponseEntity<List<RegistroDTO>> getAllRegistrosByMercancia(@PathVariable("idMercancia") String idMercancia){
+    private ResponseEntity<List<RegistroDTO>> getAllRegistrosByMercancia(@PathVariable("idMercancia") String idMercancia) {
         log.info("Ejecutando controlador Spring MVC");
         return ResponseEntity.ok(registroService.listarPorMercancia(idMercancia));
     }
-    
-    public String guardar(RegistroDTO registro){
+
+    @PostMapping
+    public String guardar(RegistroDTO registro) {
         registroService.guardar(registro);
-        return "redirect:/registro/";
+//        return "redirect:/registro/";
+        return null;
     }
 }
